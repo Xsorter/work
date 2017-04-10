2c4c323620bf01312f9c97473e0b1a71162d1f41
Xsorter <xsorter@gmail.com>
2017-04-10 22:01:53 +0300

    wamilles_landing first slide redesign

---
+++ wamilles_landing/js/src/script.js
@@ -0,0 +1,36 @@
+$(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2017, 0, 1),
+		newYear = false;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		ts = (new Date()).getTime() + 23*24*51*60*1000;
+		newYear = false;
+	}
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " дней" + ( days==1 ? '':'' ) + ", ";
+			message += hours + " часов" + ( hours==1 ? '':'' ) + ", ";
+			message += minutes + " минут" + ( minutes==1 ? '':'' ) + " и ";
+			message += seconds + " секунд" + ( seconds==1 ? '':'' ) + " <br />";
+			
+			if(newYear){
+				message += "left until the new year!";
+			}
+			else {
+				message += "";
+			}
+			
+			note.html(message);
+		}
+	});
+	
+});
