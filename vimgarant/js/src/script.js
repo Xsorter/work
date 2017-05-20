4ee284c6ed16846fdaf4bccb0d2c8eb43b3d2ab3
Xsorter <xsorter@gmail.com>
2017-05-20 18:10:10 +0300

    vimgarant markup ready

--- vimgarant/js/src/script.js
+++ vimgarant/js/src/script.js
@@ -3,3 +3,24 @@ $(function(){
 	console.log("test");
 
 });
+
+/*slider*/
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    navText:['&larr;','&rarr;'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
