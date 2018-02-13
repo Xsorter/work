c702be3012ab39aa33104d549f3dd7f1231966f0
Xsorter <xsorter@gmail.com>
2018-02-13 23:34:34 +0200

    test project init commit

---
+++ wamilles_landing-old/js/main.js
@@ -0,0 +1,363 @@
+$(function(){
+
+	// RequestAnimFrame: a browser API for getting smooth animations
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+		  window.webkitRequestAnimationFrame || 
+		  window.mozRequestAnimationFrame    || 
+		  window.oRequestAnimationFrame      || 
+		  window.msRequestAnimationFrame     ||  
+		  function( callback ){
+			window.setTimeout(callback, 1000 / 60);
+		  };
+})();
+//
+var canvas = document.getElementById("canvas");
+
+
+var ctx = canvas.getContext("2d");
+
+
+var W = window.innerWidth, H = window.innerHeight;
+canvas.width = 2000;
+canvas.height = 100;
+
+// Some variables for later use
+var particleCount = 60,
+	particles = [],
+	minDist = 70,
+	dist;
+
+// Function to paint the canvas black
+function paintCanvas() {
+	// Set the fill color to black
+	ctx.fillStyle = "rgba(255,255,255,1)";
+	
+	// This will create a rectangle of white color from the 
+	// top left (0,0) to the bottom right corner (W,H)
+	ctx.fillRect(0,0,W,H);
+}
+
+
+
+function Particle() {
+	
+	this.x = Math.random() * W;
+	this.y = Math.random() * H;
+	
+	
+	// We would also need some velocity for the particles
+	// so that they can move freely across the space
+	this.vx = -1 + Math.random() * 2;
+	this.vy = -1 + Math.random() * 2;
+
+	// Now the radius of the particles. I want all of 
+	// them to be equal in size so no Math.random() here..
+	this.radius = 4;
+	
+	
+	this.draw = function() {
+		ctx.fillStyle = "rgba(120,5,22,1)";
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		
+		// Fill the color to the arc that we just created
+		ctx.fill();
+	}
+}
+
+
+for(var i = 0; i < particleCount; i++) {
+	particles.push(new Particle());
+}
+
+
+function draw() {
+	
+	
+	paintCanvas();
+	
+	
+	for (var i = 0; i < particles.length; i++) {
+		p = particles[i];
+		p.draw();
+	}
+	
+	//Finally call the update function
+	update();
+}
+
+// Give every particle some life
+function update() {
+	
+	
+	for (var i = 0; i < particles.length; i++) {
+		p = particles[i];
+		
+		// Change the velocities
+		p.x += p.vx;
+		p.y += p.vy
+			
+		
+		if(p.x + p.radius > W) 
+			p.x = p.radius;
+		
+		else if(p.x - p.radius < 0) {
+			p.x = W - p.radius;
+		}
+		
+		if(p.y + p.radius > H) 
+			p.y = p.radius;
+		
+		else if(p.y - p.radius < 0) {
+			p.y = H - p.radius;
+		}
+		
+		
+		for(var j = i + 1; j < particles.length; j++) {
+			p2 = particles[j];
+			distance(p, p2);
+		}
+	
+	}
+}
+
+function distance(p1, p2) {
+	var dist,
+		dx = p1.x - p2.x,
+		dy = p1.y - p2.y;
+	
+	dist = Math.sqrt(dx*dx + dy*dy);
+			
+	
+	if(dist <= minDist) {
+		
+		
+		ctx.beginPath();
+		ctx.strokeStyle = "rgba(120,0,8,"+ (1.2-dist/minDist) +")";
+		ctx.moveTo(p1.x, p1.y);
+		ctx.lineTo(p2.x, p2.y);
+		ctx.stroke();
+		ctx.closePath();
+		
+		// Some acceleration for the partcles 
+		// depending upon their distance
+		var ax = dx/2000,
+			ay = dy/2000;
+		
+		// Apply the acceleration on the particles
+		p1.vx -= ax;
+		p1.vy -= ay;
+		
+		p2.vx += ax;
+		p2.vy += ay;
+	}
+}
+
+function animloop() {
+	draw();
+	requestAnimFrame(animloop);
+}
+
+animloop();
+
+//
+}); /**
+ * @name		jQuery Countdown Plugin
+ * @author		Martin Angelov
+ * @version 	1.0
+ * @url			http://tutorialzine.com/2011/12/countdown-jquery/
+ * @license		MIT License
+ */
+
+(function($){
+	
+	// Number of seconds in every time division
+	var days	= 24*60*60,
+		hours	= 60*60,
+		minutes	= 60;
+	
+	// Creating the plugin
+	$.fn.countdown = function(prop){
+		
+		var options = $.extend({
+			callback	: function(){},
+			timestamp	: 0
+		},prop);
+		
+		var left, d, h, m, s, positions;
+
+		// Initialize the plugin
+		init(this, options);
+		
+		positions = this.find('.position');
+		
+		(function tick(){
+			
+			// Time left
+			left = Math.floor((options.timestamp - (new Date())) / 1000);
+			
+			if(left < 0){
+				left = 0;
+			}
+			
+			// Number of days left
+			d = Math.floor(left / days);
+			updateDuo(0, 1, d);
+			left -= d*days;
+			
+			// Number of hours left
+			h = Math.floor(left / hours);
+			updateDuo(2, 3, h);
+			left -= h*hours;
+			
+			// Number of minutes left
+			m = Math.floor(left / minutes);
+			updateDuo(4, 5, m);
+			left -= m*minutes;
+			
+			// Number of seconds left
+			s = left;
+			updateDuo(6, 7, s);
+			
+			// Calling an optional user supplied callback
+			options.callback(d, h, m, s);
+			
+			// Scheduling another call of this function in 1s
+			setTimeout(tick, 1000);
+		})();
+		
+		// This function updates two digit positions at once
+		function updateDuo(minor,major,value){
+			switchDigit(positions.eq(minor),Math.floor(value/10)%10);
+			switchDigit(positions.eq(major),value%10);
+		}
+		
+		return this;
+	};
+
+
+	function init(elem, options){
+		elem.addClass('countdownHolder');
+
+		// Creating the markup inside the container
+		$.each(['Days','Hours','Minutes','Seconds'],function(i){
+			$('<span class="count'+this+'">').html(
+				'<span class="position">\
+					<span class="digit static">0</span>\
+				</span>\
+				<span class="position">\
+					<span class="digit static">0</span>\
+				</span>\
+				'
+			).appendTo(elem);
+			
+			if(this!="Seconds"){
+				elem.append('<span class="countDiv countDiv'+i+'"></span>');
+			}
+		});
+
+	}
+
+	// Creates an animated transition between the two numbers
+	function switchDigit(position,number){
+		
+		var digit = position.find('.digit')
+		
+		if(digit.is(':animated')){
+			return false;
+		}
+		
+		if(position.data('digit') == number){
+			// We are already showing this number
+			return false;
+		}
+		
+		position.data('digit', number);
+		
+		var replacement = $('<span>',{
+			'class':'digit',
+			css:{
+				top:'-2.1em',
+				opacity:0
+			},
+			html:number
+		});
+		
+		// The .static class is added when the animation
+		// completes. This makes it run smoother.
+		
+		digit
+			.before(replacement)
+			.removeClass('static')
+			.animate({top:'2.5em',opacity:0},'fast',function(){
+				digit.remove();
+			})
+
+		replacement
+			.delay(100)
+			.animate({top:0,opacity:1},'fast',function(){
+				replacement.addClass('static');
+			});
+	}
+})(jQuery); $(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2018, 3, 1),
+		newYear = false;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		ts = (new Date()).getTime() + 12*24*51*60*1000;
+		newYear = false;
+	}
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " дней" + ( days==1 ? '':'' ) + ", ";
+			message += hours + " часов" + ( hours==1 ? '':'' ) + ", ";
+			message += minutes + " минут" + ( minutes==1 ? '':'' ) + " и ";
+			message += seconds + " секунд" + ( seconds==1 ? '':'' ) + " <br />";
+			
+			if(newYear){
+				message += "left until the new year!";
+			}
+			else {
+				message += "";
+			}
+			
+			note.html(message);
+		}
+	});
+
+	$('#countdown2').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " дней" + ( days==1 ? '':'' ) + ", ";
+			message += hours + " часов" + ( hours==1 ? '':'' ) + ", ";
+			message += minutes + " минут" + ( minutes==1 ? '':'' ) + " и ";
+			message += seconds + " секунд" + ( seconds==1 ? '':'' ) + " <br />";
+			
+			if(newYear){
+				message += "left until the new year!";
+			}
+			else {
+				message += "";
+			}
+			
+			note.html(message);
+		}
+	});
+
+
+
+	
+});
